9e17d5badd94d51ba2b7fbf6fea739b20f518b8c
Oluwatobi22 <bakareoluwatobi22@gmail.com>
2022-07-28 22:47:51 +0100

    Ecommerce website

---
+++ Ecomm.js
@@ -0,0 +1,10 @@
+ const  filler = ()=>{
+    let take = document.getElementById("on").innerHTML;
+     document.getElementById("mm").innerHTML = take
+}
+const allIn =  ()=>{
+let allFruit = document.getElementById("all").innerHTML
+document.getElementById("space").innerHTML = allFruit
+
+}
+ 
